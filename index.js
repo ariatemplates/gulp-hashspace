6a0936ebc2dd8eff5537f39a3c2d89b7c2d76571
pk1a <pkozlowski@amadeus.com>
2014-06-25 12:03:23 +0200

    feat: inttroduce the process() method
    Closes #1

--- index.js
+++ index.js
@@ -1,77 +1,80 @@
+var path = require('path');
 var through = require('through2');
 var PluginError = require('gulp-util').PluginError;
 
 var hspCompiler = require('hashspace').compiler;
 var hspTranspiler = require('hashspace/hsp/transpiler').processString;
 
-module.exports.compile = function() {
 
-    function processHspFile(file, enc, cb){
+function compile(file, streamOfFiles) {
 
+    var compileResult = hspCompiler.compile(String(file.contents), file.path);
+    if (!compileResult.errors.length) {
+        file.contents = new Buffer(compileResult.code);
+    } else {
+        var err = compileResult.errors[0];
+        var errorMsg = 'Compilation error in "'+ file.path +'" at ' + err.line + ':' + err.column + ': ' + err.description;
+        streamOfFiles.emit('error', new PluginError('gulp-hsp', errorMsg));
+    }
+
+    return file;
+}
+
+
+function transpile(file, streamOfFiles) {
+
+    var contentAsString = String(file.contents);
+    var transpileResult = {changed: false};
+
+    try {
+        transpileResult = hspTranspiler(contentAsString, file.path);
+    } catch (e) {
+        streamOfFiles.emit('error', new PluginError('gulp-hsp',
+            'Transpilation error in "' + file.path + '" at '+ e.line + ':' + e.col + ': ' + e.message), {
+            fileName: file.path,
+            lineNumber: e.line,
+            stack: e.stack
+        });
+    }
+
+    if (transpileResult.changed) {
+        file.contents = new Buffer(transpileResult.code);
+    }
+
+    return file;
+}
+
+function gulpTaskFactory(taskToExecute) {
+
+    return function processHspFile(file, enc, cb) {
         if(file.isStream()){
-            this.emit('error', new PluginError('gulp-hsp', 'Streaming not supported'));
+            this.emit('error', new PluginError('gulp-hashspace', 'Streaming not supported'));
             return cb();
         }
 
         if(file.isBuffer()){
             try {
-                var compileResult = hspCompiler.compile(String(file.contents), file.path);
-                if (!compileResult.errors.length) {
-                    file.contents = new Buffer(compileResult.code);
-                } else {
-                    var err = compileResult.errors[0];
-                    var errorMsg = 'Compilation error in "'+ file.path +'" at ' + err.line + ':' + err.column + ': ' + err.description;
-                    this.emit('error', new PluginError('gulp-hsp', errorMsg));
-                }
+                this.push(taskToExecute(file, this));
             } catch(e) {
                 this.emit('error', e);
             }
         }
 
-        this.push(file);
         cb();
     }
+}
 
-    return through.obj(processHspFile);
+module.exports.compile = function() {
+    return through.obj(gulpTaskFactory(compile));
 };
 
 
 module.exports.transpile = function() {
+    return through.obj(gulpTaskFactory(transpile));
+};
 
-    function transpileAFile(file, enc, cb) {
-
-        if (file.isStream()) {
-            this.emit('error', new PluginError('gulp-hsp-transpiler', 'Streaming not supported'));
-            return cb();
-        }
-
-        if (file.isBuffer()) {
-            try {
-                var contentAsString = String(file.contents);
-                var transpileResult = {changed: false};
-
-                try {
-                    transpileResult = hspTranspiler(contentAsString, file.path);
-                } catch (e) {
-                    this.emit('error', new PluginError('gulp-hsp',
-                        'Transpilation error in "' + file.path + '" at '+ e.line + ':' + e.col + ': ' + e.message), {
-                        fileName: file.path,
-                        lineNumber: e.line,
-                        stack: e.stack
-                    });
-                }
-
-                if (transpileResult.changed) {
-                    file.contents = new Buffer(transpileResult.code);
-                }
-            } catch (e) {
-                this.emit('error', e);
-            }
-        }
-
-        this.push(file);
-        cb();
-    }
-
-    return through.obj(transpileAFile);
+module.exports.process = function() {
+    return through.obj(gulpTaskFactory(function(file, streamOfFiles){
+        return path.extname(file.path) === '.hsp' ? compile(file, streamOfFiles) : transpile(file, streamOfFiles);
+    }));
 };
